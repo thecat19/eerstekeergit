db26299d837df8af2742284b2550e7edcd51adbb
Dean <harrolddop@gmail.com>
2025-10-25 11:20:06 +0200

    Revert "expres even een foutje"
    This reverts commit 0f6e3dacb83c9c2edd2985fdaef96c407140e16b.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ r = tk.Tk()
 '''
 widgets are added here
 '''
-button = tk.Button(r, text='Stop', width=25, command=r.destroy
+button = tk.Button(r, text='Stop', width=25, command=r.destroy)
 button.pack()
 r.mainloop()
